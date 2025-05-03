1d28efda5e87dcfda7fd1bf41d59f5f70be1f9b9
Shohei Jinno <18493164+sjinno@users.noreply.github.com>
2025-05-03 12:24:49 -0700

    feat: display app name on home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-  return <>app</>;
+  return (
+    <>
+      <main className="px-24 py-12">
+        <h1 className="text-3xl font-bold">Job Tracking App</h1>
+      </main>
+    </>
+  );
 }
 
 export default App;
