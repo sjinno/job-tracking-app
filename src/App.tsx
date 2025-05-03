35353c74f27c293e243982898d0982879e809bb5
Shohei Jinno <18493164+sjinno@users.noreply.github.com>
2025-05-03 13:31:16 -0700

    feat: integrate `ModalContext`

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { Button } from './components/button';
+import { JobsBoard, Modal } from './components';
+import { ModalContext, useModal } from './hooks';
 
 function App() {
+  const modalState = useModal();
+
   return (
     <>
       <main className="px-24 py-12">
-        <h1 className="text-3xl font-bold">Job Tracking App</h1>
-        <div className="my-3">
-          <Button label="Add job" />
-        </div>
+        <ModalContext.Provider value={modalState}>
+          <h1 className="text-3xl font-bold">Job Tracking App</h1>
+          <JobsBoard />
+          <Modal />
+        </ModalContext.Provider>
       </main>
     </>
   );
